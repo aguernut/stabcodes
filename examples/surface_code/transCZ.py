8606beb2f2fbc68f24ef035dbd94e3e1666bf3a3
Alexandre Guernut <alexandre.guernut@loria.fr>
2025-06-27 12:45:28 +0200

    Transversal CZ Bell simulation example

--- examples/surface_code/transCZ.py
+++ examples/surface_code/transCZ.py
@@ -46,18 +46,91 @@ def SurfaceTransversalCZ(distance):
     return exp
 
 
+def SurfaceTransversalCZBell(distance):
+    code1, code2 = SurfaceCode.toric_code(distance, distance), SurfaceCode.toric_code(distance, distance)
+    perfect_code1, perfect_code2 = SurfaceCode.toric_code(distance, distance), SurfaceCode.toric_code(distance, distance)
+
+    exp = StimExperiment()
+    noise = Variable("noise")
+    exp.add_variables(noise)
+
+    exp.startup(code1, code2, perfect_code1, perfect_code2, init_bases="ZZZZ")
+
+    exp.destructive_measurement_Bell(code1.qubits, perfect_code1.qubits, "Z")
+    exp.destructive_measurement_Bell(code2.qubits, perfect_code2.qubits, "Z")
+    exp.destructive_measurement_Bell(code1.qubits, perfect_code1.qubits, "X")
+    exp.destructive_measurement_Bell(code2.qubits, perfect_code2.qubits, "X")
+
+    for i, (obs1, obs2) in enumerate(zip(code1.logical_operators["Z"], perfect_code1.logical_operators["Z"])):
+        exp.reconstruct_observable_Bell(obs1, obs2, i)
+
+    for i, (obs1, obs2) in enumerate(zip(code1.logical_operators["X"], perfect_code1.logical_operators["X"])):
+        exp.reconstruct_observable_Bell(obs1, obs2, i + 2)
+
+    for i, (obs1, obs2) in enumerate(zip(code2.logical_operators["Z"], perfect_code2.logical_operators["Z"])):
+        exp.reconstruct_observable_Bell(obs1, obs2, i + 4)
+
+    for i, (obs1, obs2) in enumerate(zip(code2.logical_operators["X"], perfect_code2.logical_operators["X"])):
+        exp.reconstruct_observable_Bell(obs1, obs2, i + 6)
+
+    exp.measure_refined_phenom(code1, meas_noise=noise, project="")
+    exp.measure_refined_phenom(code2, meas_noise=noise, project="")
+    exp.measure_refined_phenom(perfect_code1, meas_noise=0.0, project="")
+    exp.measure_refined_phenom(perfect_code2, meas_noise=0.0, project="")
+
+    for _ in range(distance // 2):
+        exp.measure_refined_phenom(code1, code2, meas_noise=noise)
+        exp.depolarize1(noise, code1.qubits)
+        exp.depolarize1(noise, code2.qubits)
+
+    mapp = mapping(code1, code2, {0: "X", 1: "Z"}, {code1.qubits[0]: code2.qubits[0]})
+    mapp2 = mapping(perfect_code1, perfect_code2, {0: "X", 1: "Z"}, {perfect_code1.qubits[0]: perfect_code2.qubits[0]})
+    exp.apply_gate("CZ", mapp.items())
+    exp.apply_gate("CZ", mapp2.items())
+    exp.depolarize2(noise, mapp.items())
+    exp.buddy_measurement(code1, code2, mapp, {0: {"X": "Z", "Z": ""}, 1: {"X": "Z", "Z": ""}}, noise, 1, 2)
+    exp.buddy_measurement(perfect_code1, perfect_code2, mapp2, {0: {"X": "Z", "Z": ""}, 1: {"X": "Z", "Z": ""}}, 0.0, 1, 2)
+
+    for _ in range(distance // 2):
+        exp.measure_refined_phenom(code1, meas_noise=noise, detector_decoration="1")
+        exp.measure_refined_phenom(code2, meas_noise=noise, detector_decoration="2")
+        exp.depolarize1(noise, code1.qubits)
+        exp.depolarize1(noise, code2.qubits)
+
+    exp.destructive_measurement_Bell(code1.qubits, perfect_code1.qubits, "Z")
+    exp.destructive_measurement_Bell(code2.qubits, perfect_code2.qubits, "Z")
+    exp.destructive_measurement_Bell(code1.qubits, perfect_code1.qubits, "X")
+    exp.destructive_measurement_Bell(code2.qubits, perfect_code2.qubits, "X")
+    exp.reconstruct_stabilizers_Bell(code1, perfect_code1, detector_decoration="1")
+    exp.reconstruct_stabilizers_Bell(code2, perfect_code2, detector_decoration="2")
+
+    for i, (obs1, obs2) in enumerate(zip(code1.logical_operators["Z"], perfect_code1.logical_operators["Z"])):
+        exp.reconstruct_observable_Bell(obs1, obs2, i)
+        exp.reconstruct_observable_Bell(obs1, obs2, i + 6)
+
+    for i, (obs1, obs2) in enumerate(zip(code1.logical_operators["X"], perfect_code1.logical_operators["X"])):
+        exp.reconstruct_observable_Bell(obs1, obs2, i + 2)
+
+    for i, (obs1, obs2) in enumerate(zip(code2.logical_operators["Z"], perfect_code2.logical_operators["Z"])):
+        exp.reconstruct_observable_Bell(obs1, obs2, i + 2)
+        exp.reconstruct_observable_Bell(obs1, obs2, i + 4)
+
+    for i, (obs1, obs2) in enumerate(zip(code2.logical_operators["X"], perfect_code2.logical_operators["X"])):
+        exp.reconstruct_observable_Bell(obs1, obs2, i + 6)
+
+    return exp
+
+
 if __name__ == "__main__":
     tasks = []
 
     custom_decoders = {}
 
-    for distance in range(3, 6, 2):
-        exp = SurfaceTransversalCZ(distance)
+    for distance in range(3, 7, 2):
+        exp = SurfaceTransversalCZBell(distance)
         t, decoders = exp.get_task(decoder=TwoStepPymatching, pass_circuit=True, d=[distance],
                                    noise=[0.01 * ((0.05 / 0.01)**(i / 10)) for i in range(11)])
         tasks.extend(t)
-        with open("foo.circ", "w") as f:
-            f.write(str(tasks[0].circuit))
         custom_decoders.update(decoders)
 
     code_stats = sinter.collect(
