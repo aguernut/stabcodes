246abd0519f386f7ffe22305f31853ce0e5a5656
Alexandre Guernut <alexandre.guernut@loria.fr>
2025-06-27 12:43:17 +0200

    Dehn twist Bell simulation example

--- examples/surface_code/dehn_twist.py
+++ examples/surface_code/dehn_twist.py
@@ -16,10 +16,6 @@ def SurfaceDT(distance):
     for i, log in enumerate(code.logical_operators["Z"]):
         exp.observable_measurement(i, log, 0.0)
 
-    # for _ in range(distance):
-    #     exp.measure_refined_phenom(code, meas_noise=noise)
-    #     exp.depolarize1(noise)
-
     aux = None
     for _ in range(distance):
         _, cnots, aux = code.dehn_twist(guide.support, aux)
@@ -34,7 +30,64 @@ def SurfaceDT(distance):
     for i, log in enumerate(code.logical_operators["Z"]):
         exp.reconstruct_observable(i, log)
 
-    exp.reconstruct_observable(1, code.logical_operators["Z"][0])
+    return exp
+
+
+def SurfaceDTBell(distance):
+    code, perfect_code = SurfaceCode.toric_code(distance, distance), SurfaceCode.toric_code(distance, distance)
+    exp = StimExperiment()
+    noise = Variable("noise")
+    exp.add_variables(noise)
+    exp.startup(code, perfect_code, init_bases="ZZ")
+
+    exp.destructive_measurement_Bell(code.qubits, perfect_code.qubits, "Z")
+    exp.destructive_measurement_Bell(code.qubits, perfect_code.qubits, "X")
+
+    exp.reconstruct_observable_Bell(code.logical_operators["Z"][0],
+                                    perfect_code.logical_operators["Z"][0], 0)
+    exp.reconstruct_observable_Bell(code.logical_operators["Z"][1],
+                                    perfect_code.logical_operators["Z"][1], 1)
+    exp.reconstruct_observable_Bell(code.logical_operators["X"][0],
+                                    perfect_code.logical_operators["X"][0], 2)
+    exp.reconstruct_observable_Bell(code.logical_operators["X"][1],
+                                    perfect_code.logical_operators["X"][1], 3)
+
+    exp.measure_refined_phenom(code, meas_noise=noise, project="")
+    exp.measure_refined_phenom(perfect_code, meas_noise=0.0, project="")
+
+    for _ in range(distance // 2):
+        exp.measure_refined_phenom(code, meas_noise=noise)
+        exp.depolarize1(noise, code.qubits)
+
+    guide = code.logical_operators["Z"][0]
+    guide2 = perfect_code.logical_operators["Z"][0]
+    aux = None
+    aux2 = None
+    for _ in range(distance):
+        _, cnots, aux = code.dehn_twist(guide.support, aux, check=False)
+        _, cnots2, aux2 = perfect_code.dehn_twist(guide2.support, aux2, check=False)
+        exp.apply_gate("CX", cnots)
+        exp.apply_gate("CX", cnots2)
+        exp.depolarize2(noise, cnots)
+        exp.measure_refined_phenom(code, meas_noise=noise)
+        exp.depolarize1(noise, support=code.qubits)
+
+    for _ in range(distance // 2):
+        exp.measure_refined_phenom(code, meas_noise=noise)
+        exp.depolarize1(noise, code.qubits)
+
+    exp.destructive_measurement_Bell(code.qubits, perfect_code.qubits, "Z")
+    exp.destructive_measurement_Bell(code.qubits, perfect_code.qubits, "X")
+    exp.reconstruct_stabilizers_Bell(code, perfect_code)
+
+    exp.reconstruct_observable_Bell(code.logical_operators["Z"][0],
+                                    perfect_code.logical_operators["Z"][0], 0)
+    exp.reconstruct_observable_Bell(code.logical_operators["Z"][1],
+                                    perfect_code.logical_operators["Z"][1], 1)
+    exp.reconstruct_observable_Bell(code.logical_operators["X"][0],
+                                    perfect_code.logical_operators["X"][0], 2)
+    exp.reconstruct_observable_Bell(code.logical_operators["X"][1],
+                                    perfect_code.logical_operators["X"][1], 3)
 
     return exp
 
@@ -44,15 +97,16 @@ if __name__ == "__main__":
     tasks = []
 
     for distance in range(3, 8, 2):
-        exp = SurfaceDT(distance)
+        exp = SurfaceDTBell(distance)
         t, _ = exp.get_task(d=[distance],
-                            noise=[0.035 * ((0.045 / 0.035)**(i / 20)) for i in range(21)])
+                            noise=[0.03 * ((0.04 / 0.03)**(i / 20)) for i in range(21)])
         tasks.extend(t)
     code_stats = sinter.collect(
         num_workers=7,
         tasks=tasks,
         decoders=["pymatching"],
-        max_shots=100_000,
+        max_shots=1_000_000,
+        print_progress=True
         # separated = True
     )
 
