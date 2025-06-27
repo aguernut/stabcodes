2e5082980a0753321cf2fc707756db30e154936b
Alexandre Guernut <alexandre.guernut@loria.fr>
2025-06-27 12:43:41 +0200

    Instantaneous Dehn twist Bell simulation example

--- examples/surface_code/instantaneous_dehn_twst.py
+++ examples/surface_code/instantaneous_dehn_twst.py
@@ -43,20 +43,86 @@ def SurfaceIDT(distance):
     return exp
 
 
+def SurfaceIDTBell(distance):
+    code, perfect_code = SurfaceCode.toric_code(distance, distance), SurfaceCode.toric_code(distance, distance)
+    exp = StimExperiment()
+    noise = Variable("noise")
+    exp.add_variables(noise)
+    exp.startup(code, perfect_code, init_bases="ZZ")
+
+    exp.destructive_measurement_Bell(code.qubits, perfect_code.qubits, "Z")
+    exp.destructive_measurement_Bell(code.qubits, perfect_code.qubits, "X")
+
+    exp.reconstruct_observable_Bell(code.logical_operators["Z"][0],
+                                    perfect_code.logical_operators["Z"][0], 0)
+    exp.reconstruct_observable_Bell(code.logical_operators["Z"][1],
+                                    perfect_code.logical_operators["Z"][1], 1)
+    exp.reconstruct_observable_Bell(code.logical_operators["X"][0],
+                                    perfect_code.logical_operators["X"][0], 2)
+    exp.reconstruct_observable_Bell(code.logical_operators["X"][1],
+                                    perfect_code.logical_operators["X"][1], 3)
+
+    exp.measure_refined_phenom(code, meas_noise=noise, project="")
+    exp.measure_refined_phenom(perfect_code, meas_noise=0.0, project="")
+
+    for _ in range(distance // 2):
+        exp.measure_refined_phenom(code, meas_noise=noise)
+        exp.depolarize1(noise, code.qubits)
+
+    c = qiskit.QuantumCircuit(len(exp._data_qubits))
+    nb_qubits = len(code.qubits)
+    supports = [(i, (i + nb_qubits // 2 + distance) % (nb_qubits // 2)) for i in range(nb_qubits // 2, nb_qubits)]
+    supports2 = [(i + nb_qubits, (i + nb_qubits // 2 + distance) % (nb_qubits // 2) + nb_qubits) for i in range(nb_qubits // 2, nb_qubits)]
+    exp.apply_gate("CX", supports)
+    exp.apply_gate("CX", supports2)
+    exp.depolarize2(noise, supports)
+
+    for qb1, qb2 in supports:
+        c.cx(qb1, qb2)
+
+    for qb1, qb2 in supports2:
+        c.cx(qb1, qb2)
+
+    code.stabilizers.apply_circuit_to_stabs(c, True)
+    code.logical_operators.apply_circuit_to_stabs(c, True)
+    perfect_code.stabilizers.apply_circuit_to_stabs(c, True)
+    perfect_code.logical_operators.apply_circuit_to_stabs(c, True)
+
+    for _ in range(distance // 2):
+        exp.measure_refined_phenom(code, meas_noise=noise)
+        exp.depolarize1(noise, code.qubits)
+
+    exp.destructive_measurement_Bell(code.qubits, perfect_code.qubits, "Z")
+    exp.destructive_measurement_Bell(code.qubits, perfect_code.qubits, "X")
+    exp.reconstruct_stabilizers_Bell(code, perfect_code)
+
+    exp.reconstruct_observable_Bell(code.logical_operators["Z"][0],
+                                    perfect_code.logical_operators["Z"][0], 0)
+    exp.reconstruct_observable_Bell(code.logical_operators["Z"][1],
+                                    perfect_code.logical_operators["Z"][1], 1)
+    exp.reconstruct_observable_Bell(code.logical_operators["X"][0],
+                                    perfect_code.logical_operators["X"][0], 2)
+    exp.reconstruct_observable_Bell(code.logical_operators["X"][1],
+                                    perfect_code.logical_operators["X"][1], 3)
+
+    return exp
+
+
 if __name__ == "__main__":
 
     tasks = []
 
     for distance in range(3, 8, 2):
-        exp = SurfaceIDT(distance)
+        exp = SurfaceIDTBell(distance)
         t, _ = exp.get_task(d=[distance],
-                            noise=[0.035 * ((0.045 / 0.035)**(i / 20)) for i in range(21)])
+                            noise=[0.025 * ((0.035 / 0.025)**(i / 20)) for i in range(21)])
         tasks.extend(t)
     code_stats = sinter.collect(
         num_workers=7,
         tasks=tasks,
         decoders=["pymatching"],
         max_shots=100_000,
+        print_progress=True
         # separated = True
     )
 
