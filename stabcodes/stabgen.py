494a1a1ad4bda88b80f569cd6d480c8a287a802f
Alexandre Guernut <alexandre.guernut@loria.fr>
2025-07-29 17:56:33 +0200

    Documented Stabgen

--- stabcodes/stabgen.py
+++ stabcodes/stabgen.py
@@ -1,19 +1,70 @@
 """
-
+Specialization of the :obj:`RecursiveMapping` data structure to hold stabilizers and Pauli operators.
+The intent is to have the stabilizers accessible both through indexing or filtering their Pauli type or possibly their color.
 """
 
 from typing import Optional, Union
 from collections.abc import Sequence, Mapping
 from stabcodes.recursivemapping import RecursiveMapping
-from stabcodes.pauli import PauliOperator, Stabilizer2D
+from stabcodes.pauli import PauliOperator, Stabilizer, Stabilizer2D, X, Z
 from qiskit import QuantumCircuit
 import numpy as np
 from numpy.typing import NDArray
 
 
 class StabGen(RecursiveMapping):
+    """
+    A :obj:`RecursiveMapping` designed to hold :obj:`PauliOperator`s and :obj:`Stabilizer`s
+    in the context of a stabilizer code.
+
+    Elements are stored hierarchically with string labels characterizing them
+    (such as "X" or "Z" for CSS codes, "red" or "blue" or "green" as a second
+     level indexing for color code, etc).
+
+    Notes
+    -----
+    As the labels are lexicographically comparable, the stabilizers are always iterated
+    over in the same order, as long as no label changes and no elements is added.
+
+    Examples
+    --------
+    >>> stabs = StabGen({"X": [Stabilizer.from_support([0, 1], "X", 3), Stabilizer.from_support([1, 2], "X", 3)], "Z": [Stabilizer.from_support([0, 1, 2], "Z", 3)]})
+    >>> stabs
+    StabGen({'X': StabGen([Stabilizer([X(0), X(1)], 3), Stabilizer([X(1), X(2)], 3)]), 'Z': StabGen([Stabilizer([Z(0), Z(1), Z(2)], 3)])})
+    >>> colored_stabs = StabGen({"red": stabs, "blue": {"X": [Stabilizer.from_support([0, 2], "X", 3)]}})
+    >>> colored_stabs
+    StabGen({'blue': StabGen({'X': StabGen([Stabilizer([X(0), X(2)], 3)])}), 'red': StabGen({'X': StabGen([Stabilizer([X(0), X(1)], 3), Stabilizer([X(1), X(2)], 3)]), 'Z': StabGen([Stabilizer([Z(0), Z(1), Z(2)], 3)])})})
+
+    """
 
     def __init__(self, value: Optional[Union[Sequence[PauliOperator], Mapping[str, PauliOperator]]] = None):
+        """Builds a StabGen from a sequence or a possibly recursive uniform mapping, ending with sequences of :obj:`Paulioperator`s.
+
+        Parameters
+        ----------
+        value: Union[Sequence[PauliOperator], Mapping[str, PauliOperator]], optional
+            When :onj:`None`, builds the empty :onj:`StabGen`.
+            When given a sequence of :obj:`PauliOperator`s, builds a base level :obj:`StabGen` containing its elements.
+            When given a :obj:`StabGen`, performs a shallow copy.
+            When given a mapping, recursively calls itself to build the :obj:`StabGen`.
+
+        Raises
+        ------
+        TypeError
+            Raised when non-uniform keys would be used, or a non-:obj:`PauliOperator` element is given.
+        ValueError
+            Raised if some elements do not affect the same number of qubits.
+
+        Examples
+        --------
+        >>> stabs = StabGen({"X": [Stabilizer.from_support([0, 1], "X", 3), Stabilizer.from_support([1, 2], "X", 3)], "Z": [Stabilizer.from_support([0, 1, 2], "Z", 3)]})
+        >>> stabs
+        StabGen({'X': StabGen([Stabilizer([X(0), X(1)], 3), Stabilizer([X(1), X(2)], 3)]), 'Z': StabGen([Stabilizer([Z(0), Z(1), Z(2)], 3)])})
+        >>> colored_stabs = StabGen({"red": stabs, "blue": {"X": [Stabilizer.from_support([0, 2], "X", 3)]}})
+        >>> colored_stabs
+        StabGen({'blue': StabGen({'X': StabGen([Stabilizer([X(0), X(2)], 3)])}), 'red': StabGen({'X': StabGen([Stabilizer([X(0), X(1)], 3), Stabilizer([X(1), X(2)], 3)]), 'Z': StabGen([Stabilizer([Z(0), Z(1), Z(2)], 3)])})})
+
+        """
         super().__init__(value)
 
         nb_qubits = None
@@ -23,15 +74,47 @@ class StabGen(RecursiveMapping):
             if nb_qubits is None:
                 nb_qubits = s.nb_qubits
             elif nb_qubits != s.nb_qubits:
-                raise ValueError(f"Stabilizer {repr(s)} does not affect {nb_qubits} qubits")
+                raise ValueError(f"{repr(s)} does not affect {nb_qubits} qubits")
 
         self._nb_qubits = nb_qubits
 
     @property
     def nb_qubits(self) -> int:
+        """Number of qubits affected by the elements."""
         return self._nb_qubits
 
     def apply_circuit_to_stabs(self, circuit: QuantumCircuit, _override_safeguard: bool = False):
+        """Apply the given circuit to all the base-level :obj:`PauliOperator` by conjugation.
+
+        Parameters
+        ----------
+        circuit: :class:`~qiskit.circuit.QuantumCircuit`
+            Quantum circuit to apply.
+        _override_safeguard : bool, optional
+            Whether to bypass the safeguard ensuring that an entangling two-qubit cannot silently be applied.
+
+        Notes
+        -----
+        If `_override_safeguard` is set to True, the :obj:`Stabilizer2D` will not be
+        in a coherent state and will have to be manually updated.
+
+        Examples
+        --------
+        >>> import qiskit
+        >>> P = Stabilizer2D([Z(1)], 2)
+        >>> Q = Stabilizer2D([X(0)], 2)
+        >>> S = StabGen({'X': [Q], 'Z': [P]})
+        >>> circuit = qiskit.QuantumCircuit(2)
+        >>> circuit.h(0)
+        <...
+        >>> circuit.cx(0, 1)
+        <...
+        >>> S.apply_circuit_to_stabs(circuit, True)
+        >>> S['Z'][0].order = [0, 1]
+        >>> S
+        StabGen({'X': StabGen([Stabilizer2D([Z(0)], 2)]), 'Z': StabGen([Stabilizer2D([Z(0), Z(1)], 2)])})
+
+        """
         for stab in self:
             if isinstance(stab, Stabilizer2D):
                 stab.apply_circuit(circuit, _override_safeguard)
@@ -39,6 +122,7 @@ class StabGen(RecursiveMapping):
                 stab.apply_circuit(circuit)
 
     def to_matrix(self) -> NDArray[np.int64]:
+        """Returns a view of this object as a matrix of this symplectic view of its elements."""
         mat = np.zeros((len(self), self._nb_qubits * 2))
 
         for (i, stab) in enumerate(self):
@@ -47,6 +131,14 @@ class StabGen(RecursiveMapping):
         return mat
 
     def gram_product(self, other: "StabGen") -> NDArray[np.int64]:
+        """Builds the gram-like matrix of the symplectic bilinear for to summarize the commutativity relation between all the stored elements of both :class:`StabGen`.
+
+        Parameters
+        ----------
+        other: StabGen
+            Stabgen from which to compute commutativity.
+
+        """
         lambd = np.zeros((self._nb_qubits * 2, self._nb_qubits * 2))
         lambd[:self._nb_qubits, self._nb_qubits:] = np.eye(self.nb_qubits)
         lambd[self._nb_qubits:, :self._nb_qubits] = np.eye(self.nb_qubits)
@@ -54,14 +146,40 @@ class StabGen(RecursiveMapping):
         return (self.to_matrix() @ lambd @ other.to_matrix().T) % 2
 
     def reset(self):
+        """Resets the stored elements so that they can take part of a new simulation."""
         for s in self:
             s.reset()
 
     def extend(self, n: int):
+        """Extends the domain of all the stored elements.
+
+        Parameters
+        ----------
+        n: int
+            Number of additional qubits to consider. Must be positive.
+
+        """
         for s in self:
             s.extend(n)
 
     def itranslate(self, n: Optional[int] = None, mapping: Optional[dict[int, int]] = None):
+        """Translates in-place the stored elements.
+
+        Parameters
+        ----------
+        mapping : dict[int, int], optional
+            Remapping of the qubits
+        n : int, optional
+            "Space" translation of the qubit indices.
+
+        Notes
+        -----
+        Both arguments cannot be specified at the same time.
+        The mapping argument will take priority. This method
+        should be combined to :meth:`extend` if one wants both
+        functionalities.
+
+        """
         for i, s in enumerate(self):
             self[i] = s.translate(n=n, mapping=mapping)
 
@@ -69,19 +187,3 @@ class StabGen(RecursiveMapping):
 if __name__ == "__main__":
     import doctest
     doctest.testmod(optionflags=doctest.ELLIPSIS)
-
-    stabs = [PauliOperator.from_str("+IXZZXI"),
-             PauliOperator.from_str("+XZZXII"),
-             PauliOperator.from_str("+ZZXIXI"),
-             PauliOperator.from_str("+ZXIXZI"),
-             PauliOperator.from_str("+XIXZZI")]
-
-    log = {"X": [PauliOperator.from_str("+XXXXXI"), PauliOperator.from_str("+IIIIIX")],
-           "Z": [PauliOperator.from_str("+ZZZZZI"), PauliOperator.from_str("+IIIIIZ")]}
-
-    stabs = StabGen(stabs)
-    log = StabGen(log)
-
-    print(stabs.to_matrix())
-    print(log.to_matrix())
-    print(log.gram_product(log))
